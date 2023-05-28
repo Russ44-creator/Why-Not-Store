1e35d83b8a31fa9c6acb57b5219741b3f6a717c5
Russ_Deng <1254016017@qq.com>
2023-05-28 20:45:06 +0800

    add styles.jsx

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,37 +9,65 @@ import { signOut } from "firebase/auth";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { CartContext } from "../../contexts/cart.context";
+import './navigation.styles.scss';
 
 const Navigation = () => {
     const { currentUser} = useContext(UserContext);
     const { isCartOpen } = useContext(CartContext);
 
     return (
-      <Fragment>
-        <NavigationContainer>
-            <LogoContainer to="/">
-                <Toplogo className="logo" />
-            </LogoContainer>           
-            <NavLinksContainer>
-                <NavLink to='/shop'>
-                    SHOP
-                </NavLink>
-                {
-                    currentUser ? (
-                        <NavLink as='span' onClick={signOutUser}>
-                            SIGN OUT</NavLink>
-                    )
-                        : (<NavLink to="/auth">
-                            SIGN IN
-                        </NavLink>
-                        )
-                }
-                <CartIcon />
-            </NavLinksContainer>
-            {isCartOpen && <CartDropdown />}
-        </NavigationContainer>
-        <Outlet />
-      </Fragment>
+    //   <Fragment>
+    //     <NavigationContainer>
+    //         <LogoContainer to="/">
+    //             <Toplogo className="logo" />
+    //         </LogoContainer>           
+    //         <NavLinksContainer>
+    //             <NavLink to='/shop'>
+    //                 SHOP
+    //             </NavLink>
+    //             {
+    //                 currentUser ? (
+    //                     <NavLink as='span' onClick={signOutUser}>
+    //                         SIGN OUT</NavLink>
+    //                 )
+    //                     : (<NavLink to="/auth">
+    //                         SIGN IN
+    //                     </NavLink>
+    //                     )
+    //             }
+    //             <CartIcon />
+    //         </NavLinksContainer>
+    //         {isCartOpen && <CartDropdown />}
+    //     </NavigationContainer>
+    //     <Outlet />
+    //   </Fragment>
+    <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img className="logo" src={logo} alt="Logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          {currentUser ? (
+            <>
+              <span className="nav-link" style={{ cursor: 'inherit' }}>
+                Hello,{currentUser.displayName}.
+              </span>
+              <span className="nav-link" onClick={signOutUser}>
+                SIGN OUT
+              </span>
+            </>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
+
+          <CartIcon />
+        </div>
+        {isCartOpen && <CartDropdown />}
+      </div>
     );
   };
 
