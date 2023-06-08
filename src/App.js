2ce0d2b37b635c2cd45127bf4d3a47cd3afec148
Russ_Deng <1254016017@qq.com>
2023-06-08 17:31:38 +0800

    add useselector

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
           <Route path='auth' element={<Authentication />} />
           <Route path='checkout' element={<Checkout />} />
         
-        </Route>
+      </Route>
     </Routes>
   );
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,7 +3,7 @@ import CategoriesPreview from "../categories-preview/categories-preview.componen
 import Category from "../category/category.component";
 import { useEffect } from "react";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.action";
 import { useDispatch } from "react-redux";
 
 
@@ -12,9 +12,9 @@ const Shop = () => {
 
     useEffect(() => {
         const getCategoriesMap = async () => {
-            const categoryMap = await getCategoriesAndDocuments();
-            dispatch(setCategoriesMap(categoryMap));
-        }
+            const categoriesArray = await getCategoriesAndDocuments('categories');
+            dispatch(setCategories(categoriesArray));
+        };
         getCategoriesMap();
     }, [])
 
